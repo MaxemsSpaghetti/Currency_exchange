1c037423483b510ebfe6db9e2ff6d31c15eea825
MaxemsSpaghetti <maxbuten00@mail.ru>
2023-10-25 22:12:27 +0300

    feat: add servlet /currencies

---
+++ src/main/java/maxim/butenko/servlet/CurrenciesServlet.java
@@ -0,0 +1,34 @@
+package maxim.butenko.servlet;
+
+import maxim.butenko.service.CurrencyServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet("/currencies")
+public class CurrenciesServlet extends HttpServlet {
+
+    private final CurrencyServiceImpl currencyService = CurrencyServiceImpl.getInstance();
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        try(var printWriter = resp.getWriter()) {
+            printWriter.write("<h1>Currencies</h1>");
+            printWriter.write("<ul>");
+            currencyService.findAll().forEach(currencyDTO -> {
+                        printWriter.write("<li>");
+                        printWriter.write(currencyDTO.getId() + "/n "
+                                + currencyDTO.getCode() + "/n "
+                                + currencyDTO.getFullName() + "/n "
+                                + currencyDTO.getSign());
+                    });
+            printWriter.write("</ul>");
+        }
+    }
+}
