23b59ae6270962ba04d092e92a14175c831a4771
MaxemsSpaghetti <maxbuten00@mail.ru>
2023-10-26 21:36:36 +0300

    feat: add CurrencyService and method findAll

--- src/main/java/maxim/butenko/service/CurrencyService.java
+++ src/main/java/maxim/butenko/service/CurrencyService.java
@@ -1,10 +1,34 @@
 package maxim.butenko.service;
 
+import maxim.butenko.dao.CurrencyDAO;
 import maxim.butenko.dto.CurrencyDTO;
 
 import java.util.List;
 
-public interface CurrencyService {
+import static java.util.stream.Collectors.toList;
 
-    List<CurrencyDTO> findAll();
+public class CurrencyService implements Service<CurrencyDTO> {
+
+    private final CurrencyDAO currencyDAO = CurrencyDAO.getInstance();
+
+    public static final CurrencyService INSTANCE = new CurrencyService();
+
+    private CurrencyService() {
+
+    }
+
+    @Override
+    public List<CurrencyDTO> findAll() {
+        return currencyDAO.findAll().stream()
+                .map(currency -> new CurrencyDTO(
+                        currency.getId(),
+                        currency.getCode(),
+                        currency.getFullName(),
+                        currency.getSign()))
+                .collect(toList());
+    }
+
+    public static CurrencyService getInstance() {
+        return INSTANCE;
+    }
 }
