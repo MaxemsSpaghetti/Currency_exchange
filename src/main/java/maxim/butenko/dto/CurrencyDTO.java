a94f910ef602e39baa44e2468f4724eea32c1bec
MaxemsSpaghetti <maxbuten00@mail.ru>
2023-10-28 15:50:02 +0300

    fix: add lombok annotations to CurrencyDTO

--- src/main/java/maxim/butenko/dto/CurrencyDTO.java
+++ src/main/java/maxim/butenko/dto/CurrencyDTO.java
@@ -1,46 +1,13 @@
 package maxim.butenko.dto;
 
-import java.util.Objects;
+import lombok.Builder;
+import lombok.Value;
 
+@Value
+@Builder
 public class CurrencyDTO {
-    private final Long id;
-    private final String code;
-    private final String fullName;
-    private final String sign;
-
-    public CurrencyDTO(Long id, String code, String fullName, String sign) {
-        this.id = id;
-        this.code = code;
-        this.fullName = fullName;
-        this.sign = sign;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public String getSign() {
-        return sign;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        CurrencyDTO that = (CurrencyDTO) o;
-        return Objects.equals(id, that.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
+    Long id;
+    String code;
+    String fullName;
+    String sign;
 }
