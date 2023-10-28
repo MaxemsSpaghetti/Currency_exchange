d853cc80be83e369868b7235c9e732d6028365a7
MaxemsSpaghetti <maxbuten00@mail.ru>
2023-10-28 15:49:38 +0300

    fix: add lombok annotations to ExchangeRateDTO

--- src/main/java/maxim/butenko/dto/ExchangeRateDTO.java
+++ src/main/java/maxim/butenko/dto/ExchangeRateDTO.java
@@ -1,57 +1,12 @@
 package maxim.butenko.dto;
 
-import java.util.Objects;
-
+import lombok.Builder;
+import lombok.Value;
+@Value
+@Builder
 public class ExchangeRateDTO {
-
-    private final long id;
-    private final int baseCurrencyId;
-    private final int targetCurrencyId;
-    private final double rate;
-
-    public ExchangeRateDTO(long id, int baseCurrencyId, int targetCurrencyId, double rate) {
-        this.id = id;
-        this.baseCurrencyId = baseCurrencyId;
-        this.targetCurrencyId = targetCurrencyId;
-        this.rate = rate;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public int getBaseCurrencyId() {
-        return baseCurrencyId;
-    }
-
-    public int getTargetCurrencyId() {
-        return targetCurrencyId;
-    }
-
-    public double getRate() {
-        return rate;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ExchangeRateDTO that = (ExchangeRateDTO) o;
-        return id == that.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    @Override
-    public String toString() {
-        return "ExchangeRateDTO{" +
-                "id=" + id +
-                ", baseCurrencyId=" + baseCurrencyId +
-                ", targetCurrencyId=" + targetCurrencyId +
-                ", rate=" + rate +
-                '}';
-    }
+    long id;
+    int baseCurrencyId;
+    int targetCurrencyId;
+    double rate;
 }
