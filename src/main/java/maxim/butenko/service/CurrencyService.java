82a414cabb9d427da0c8de98b3041c6745de92e2
MaxemsSpaghetti <maxbuten00@mail.ru>
2023-10-29 13:33:05 +0300

    fix: add method buildCurrency and create and refactor findAll method

--- src/main/java/maxim/butenko/service/CurrencyService.java
+++ src/main/java/maxim/butenko/service/CurrencyService.java
@@ -2,6 +2,7 @@ package maxim.butenko.service;
 
 import maxim.butenko.dao.CurrencyDAOImpl;
 import maxim.butenko.dto.CurrencyDTO;
+import maxim.butenko.model.Currency;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,27 +21,33 @@ public class CurrencyService {
 
     public List<CurrencyDTO> findAll() {
         return currencyDAO.findAll().stream()
-                .map(currency -> CurrencyDTO.builder()
-                        .id(currency.getId())
-                        .fullName(currency.getFullName())
-                        .code(currency.getCode())
-                        .sign(currency.getSign())
-                        .build())
+                .map(this::buildCurrency)
                 .collect(toList());
     }
 
     public Optional<CurrencyDTO> findByCode(String code) {
         return currencyDAO.findByCode(code).stream()
-                .map(currency -> CurrencyDTO.builder()
-                        .id(currency.getId())
-                        .fullName(currency.getFullName())
-                        .code(currency.getCode())
-                        .sign(currency.getSign())
-                        .build())
+                .map(this::buildCurrency)
                 .findFirst();
     }
 
     public static CurrencyService getInstance() {
         return INSTANCE;
     }
+
+    public Optional<CurrencyDTO> create( String fullName, String code, String sign) {
+        var currency = new Currency(null, fullName, code, sign);
+        currencyDAO.save(currency);
+        return Optional.ofNullable(buildCurrency(currency));
+    }
+
+    private CurrencyDTO buildCurrency(Currency currency) {
+        return CurrencyDTO.builder()
+                .id(currency.getId())
+                .fullName(currency.getFullName())
+                .code(currency.getCode())
+                .sign(currency.getSign())
+                .build();
+    }
+
 }
