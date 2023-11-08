e67cc3aa4829ef0703c4010d14ba3d576d6c2a95
MaxemsSpaghetti <maxbuten00@mail.ru>
2023-11-08 19:16:22 +0300

    feat: add method findByCodes

--- src/main/java/maxim/butenko/dao/ExchangeRateDAO.java
+++ src/main/java/maxim/butenko/dao/ExchangeRateDAO.java
@@ -2,7 +2,8 @@ package maxim.butenko.dao;
 
 import maxim.butenko.model.ExchangeRate;
 
+import java.util.Optional;
+
 public interface ExchangeRateDAO extends DAO<ExchangeRate> {
-
-
+    Optional<ExchangeRate> findByCodes(String baseCurrencyCode, String targetCurrencyCode);
 }
