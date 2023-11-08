9788f44cc2bebe878c67b65e09ea01f0a43e3157
MaxemsSpaghetti <maxbuten00@mail.ru>
2023-11-08 19:13:41 +0300

    fix: add try with resources in methods and add enum of sql queries

--- src/main/java/maxim/butenko/dao/CurrencyDAOImpl.java
+++ src/main/java/maxim/butenko/dao/CurrencyDAOImpl.java
@@ -13,12 +13,6 @@ public class CurrencyDAOImpl implements CurrencyDAO {
 
     private static final CurrencyDAOImpl INSTANCE = new CurrencyDAOImpl();
 
-    private static final String FIND_ALL = "SELECT * FROM currencies";
-
-    private static final String FIND_BY_CODE = "SELECT * FROM currencies WHERE code = ?";
-
-    private static final String CREATE = "INSERT INTO currencies (fullName, code, sign) VALUES (?, ?, ?)";
-
     private CurrencyDAOImpl() {
 
     }
@@ -30,66 +24,82 @@ public class CurrencyDAOImpl implements CurrencyDAO {
     @SneakyThrows
     private Currency buildCurrency(ResultSet resultSet) {
         return new Currency(
-                resultSet.getObject("id", Long.class),
-                resultSet.getObject("fullName", String.class),
-                resultSet.getObject("code", String.class),
-                resultSet.getObject("sign", String.class)
+                resultSet.getLong("id"),
+                resultSet.getString("fullName"),
+                resultSet.getString("code"),
+                resultSet.getString("sign")
         );
     }
 
     @Override
     @SneakyThrows
     public List<Currency> findAll() {
-        var connection = ConnectionManager.get();
-        var prepareStatement  = connection.prepareStatement(FIND_ALL);
-        var resultSet = prepareStatement.executeQuery();
-        List<Currency> currencies = new ArrayList<>();
-        while (resultSet.next()) {
-            currencies.add(buildCurrency(resultSet));
+        try (var connection = ConnectionManager.get()){
+            var prepareStatement = connection.prepareStatement(SQLQuery.FIND_ALL.QUERY);
+            var resultSet = prepareStatement.executeQuery();
+            List<Currency> currencies = new ArrayList<>();
+            while (resultSet.next()) {
+                currencies.add(buildCurrency(resultSet));
+            }
+            return currencies;
         }
-        return currencies;
     }
 
     @Override
     @SneakyThrows
     public Optional<Currency> findByCode(String code) {
-        var connection = ConnectionManager.get();
-        var prepareStatement = connection.prepareStatement(FIND_BY_CODE);
-        prepareStatement.setString(1, code);
-        var result = prepareStatement.executeQuery();
+        try (var connection = ConnectionManager.get()) {
+            var prepareStatement = connection.prepareStatement(SQLQuery.FIND_BY_CODE.QUERY);
 
-        if (!result.next()) {
-            return Optional.empty();
+            prepareStatement.setString(1, code);
+            var result = prepareStatement.executeQuery();
+
+            if (!result.next()) {
+                return Optional.empty();
+            }
+            return Optional.of(buildCurrency(result));
         }
-        return Optional.of(buildCurrency(result));
     }
 
 
     @Override
-    public void update(Currency currency) {
-
+    public Currency update(Currency currency) {
+        return null;
     }
 
     @Override
     @SneakyThrows
     public Currency save(Currency currency) {
-        var connection = ConnectionManager.get();
-        var prepareStatement = connection.prepareStatement(CREATE);
-        prepareStatement.setString(1, currency.getFullName());
-        prepareStatement.setString(2, currency.getCode());
-        prepareStatement.setString(3, currency.getSign());
-        var resultSet = prepareStatement.executeUpdate();
-        if (resultSet == 0) {
-            return null;
-        }
-        try (var generatedKeys = prepareStatement.getGeneratedKeys()) {
-            if (generatedKeys.next()) {
-                var generatedId = generatedKeys.getLong(1);
-                currency.setId(generatedId);
-            } else {
+        try (var connection = ConnectionManager.get()){
+            var prepareStatement = connection.prepareStatement(SQLQuery.CREATE.QUERY);
+            prepareStatement.setString(1, currency.getFullName());
+            prepareStatement.setString(2, currency.getCode());
+            prepareStatement.setString(3, currency.getSign());
+            var executeUpdate = prepareStatement.executeUpdate();
+            if (executeUpdate == 0) {
                 return null;
             }
+            try (var generatedKeys = prepareStatement.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    var generatedId = generatedKeys.getLong(1);
+                    currency.setId(generatedId);
+                } else {
+                    return null;
+                }
+            }
+            return currency;
         }
-        return currency;
+    }
+
+    enum SQLQuery {
+        FIND_ALL("SELECT * FROM currencies"),
+        FIND_BY_CODE("SELECT * FROM currencies WHERE code = ?"),
+        CREATE("INSERT INTO currencies (fullName, code, sign) VALUES (?, ?, ?)");
+
+        String QUERY;
+
+        SQLQuery(String query) {
+            this.QUERY = query;
+        }
     }
 }
