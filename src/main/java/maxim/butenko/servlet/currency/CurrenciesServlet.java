1ba71118c512d77761a361c6ee827f1ee24903b4
MaxemsSpaghetti <maxbuten00@mail.ru>
2023-11-09 22:14:30 +0300

    feat: add exception handling in method doPost

--- src/main/java/maxim/butenko/servlet/currency/CurrenciesServlet.java
+++ src/main/java/maxim/butenko/servlet/currency/CurrenciesServlet.java
@@ -3,14 +3,14 @@ package maxim.butenko.servlet.currency;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import maxim.butenko.dto.CurrencyDTO;
 import maxim.butenko.service.CurrencyService;
+import org.sqlite.SQLiteErrorCode;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,16 +21,16 @@ public class CurrenciesServlet extends HttpServlet {
     private final CurrencyService currencyService = CurrencyService.getInstance();
     private final ObjectMapper objectMapper = new ObjectMapper();
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
         try {
             resp.setContentType("application/json");
-            List<CurrencyDTO> currencies = currencyService.findAll();
-            String json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(currencies);
 
-            PrintWriter printWriter = resp.getWriter();
-            printWriter.write(json);
-        } catch (IOException e) {
+            List<CurrencyDTO> currencies = currencyService.findAll();
+
+            String json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(currencies);
+            resp.getWriter().write(json);
+        } catch (SQLException e) {
             resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
                     "the database is unavailable now, sorry(");
@@ -39,20 +39,51 @@ public class CurrenciesServlet extends HttpServlet {
 
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
-        resp.setContentType("application/json");
+            resp.setContentType("application/json");
 
-        String fullName = req.getParameter("fullName");
-        String code = req.getParameter("code");
-        String sign = req.getParameter("sign");
+            String fullName = req.getParameter("fullName");
+            String code = req.getParameter("code");
+            String sign = req.getParameter("sign");
 
-        Optional<CurrencyDTO> newCurrency = currencyService.create(fullName, code, sign);
-        if (newCurrency.isPresent()) {
-            var currencyDTO = newCurrency.get();
-            String json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(currencyDTO);
-            try (var printWriter = resp.getWriter()) {
-                printWriter.write(json);
+            if (fullName.isBlank()) {
+                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                resp.sendError(HttpServletResponse.SC_BAD_REQUEST,
+                        "fullName is required");
+                return;
+            } else if (code.isBlank()) {
+                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                resp.sendError(HttpServletResponse.SC_BAD_REQUEST,
+                        "code is required");
+                return;
+            } else if (code.length() != 3 || !code.matches("[A-Z]{3}")) {
+                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                resp.sendError(HttpServletResponse.SC_BAD_REQUEST,
+                        "Incorrect currency code entry. Currency code must consist of 3 letters in uppercase");
+                return;
+            } else if (sign.isBlank()) {
+                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                resp.sendError(HttpServletResponse.SC_BAD_REQUEST,
+                        "sign is required");
+                return;
+            }
+        try {
+            Optional<CurrencyDTO> newCurrency = currencyService.create(fullName, code, sign);
+            if (newCurrency.isPresent()) {
+                CurrencyDTO currencyDTO = newCurrency.get();
+                String json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(currencyDTO);
+                resp.getWriter().write(json);
+                }
+        } catch (SQLException e) {
+            if (e.getErrorCode() == SQLiteErrorCode.SQLITE_CONSTRAINT.code) {
+                resp.setStatus(HttpServletResponse.SC_CONFLICT);
+                resp.sendError(HttpServletResponse.SC_CONFLICT,
+                        "Currency with this code already exists");
+            } else {
+                resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+                        "the database is unavailable now, sorry(");
             }
         }
     }
