c28338b8fd12f206a4eddb97d225572b9e04c6db
MaxemsSpaghetti <maxbuten00@mail.ru>
2023-11-09 22:15:06 +0300

    fix: add exception throwing

--- src/main/java/maxim/butenko/dao/CurrencyDAO.java
+++ src/main/java/maxim/butenko/dao/CurrencyDAO.java
@@ -2,9 +2,10 @@ package maxim.butenko.dao;
 
 import maxim.butenko.model.Currency;
 
+import java.sql.SQLException;
 import java.util.Optional;
 
 public interface CurrencyDAO extends DAO<Currency> {
 
-    Optional<Currency> findByCode(String code);
+    Optional<Currency> findByCode(String code) throws SQLException;
 }
