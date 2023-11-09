094ef92f1c21ff6c2c5e8bd69301eabd7b69aad2
MaxemsSpaghetti <maxbuten00@mail.ru>
2023-11-09 22:16:19 +0300

    fix: add SQLException throwing

--- src/main/java/maxim/butenko/dao/ExchangeRateDAO.java
+++ src/main/java/maxim/butenko/dao/ExchangeRateDAO.java
@@ -2,8 +2,9 @@ package maxim.butenko.dao;
 
 import maxim.butenko.model.ExchangeRate;
 
+import java.sql.SQLException;
 import java.util.Optional;
 
 public interface ExchangeRateDAO extends DAO<ExchangeRate> {
-    Optional<ExchangeRate> findByCodes(String baseCurrencyCode, String targetCurrencyCode);
+    Optional<ExchangeRate> findByCodes(String baseCurrencyCode, String targetCurrencyCode) throws SQLException;
 }
