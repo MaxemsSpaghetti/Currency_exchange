ef9f351ca313bd6e60ea838e647b70614b16dc11
MaxemsSpaghetti <maxbuten00@mail.ru>
2023-11-12 20:26:33 +0300

    feat: add convertCurrency method

--- src/main/java/maxim/butenko/service/ExchangeRateService.java
+++ src/main/java/maxim/butenko/service/ExchangeRateService.java
@@ -3,6 +3,8 @@ package maxim.butenko.service;
 import maxim.butenko.dao.ExchangeRateDAOImpl;
 import maxim.butenko.dto.CurrencyDTO;
 import maxim.butenko.dto.ExchangeRateDTO;
+import maxim.butenko.model.Currency;
+import maxim.butenko.model.CurrencyExchange;
 import maxim.butenko.model.ExchangeRate;
 
 import java.sql.SQLException;
@@ -44,31 +46,36 @@ public class ExchangeRateService {
 
     public Optional<ExchangeRateDTO> create(
             String baseCurrencyCode, String targetCurrencyCode, Double rate) throws SQLException {
-        var baseCurrencyDTO = currencyService.findByCode(baseCurrencyCode).orElseThrow();
-        var targetCurrencyDTO = currencyService.findByCode(targetCurrencyCode).orElseThrow();
+        CurrencyDTO baseCurrencyDTO = currencyService.findByCode(baseCurrencyCode).orElseThrow();
+        CurrencyDTO targetCurrencyDTO = currencyService.findByCode(targetCurrencyCode).orElseThrow();
 
-        var baseCurrency = currencyMapper.convertCurrencyDTOToCurrency(baseCurrencyDTO);
-        var targetCurrency = currencyMapper.convertCurrencyDTOToCurrency(targetCurrencyDTO);
+        Currency baseCurrency = currencyMapper.convertCurrencyDTOToCurrency(baseCurrencyDTO);
+        Currency targetCurrency = currencyMapper.convertCurrencyDTOToCurrency(targetCurrencyDTO);
 
         ExchangeRate save = exchangeRateDAO.save(new ExchangeRate(null, baseCurrency, targetCurrency, rate));
+
         return Optional.ofNullable(buildExchangeRate(save));
     }
 
     public Optional<ExchangeRateDTO> update(
             String baseCurrencyCode, String targetCurrencyCode, Double rate) throws SQLException {
         Optional<ExchangeRate> rateBeforeUpdate = exchangeRateDAO.findByCodes(baseCurrencyCode, targetCurrencyCode);
+
         if (rateBeforeUpdate.isEmpty()) {
             return Optional.empty();
         }
+
         ExchangeRate exchangeRate = rateBeforeUpdate.get();
         exchangeRate.setRate(rate);
         ExchangeRate update = exchangeRateDAO.update(exchangeRate);
+
         return Optional.ofNullable(buildExchangeRate(update));
     }
 
     private ExchangeRateDTO buildExchangeRate(ExchangeRate exchangeRate) {
         CurrencyDTO baseCurrency = currencyMapper.convertCurrencyToCurrencyDTO(exchangeRate.getBaseCurrency());
         CurrencyDTO targetCurrency = currencyMapper.convertCurrencyToCurrencyDTO(exchangeRate.getTargetCurrency());
+
         return ExchangeRateDTO.builder()
                 .id(exchangeRate.getId())
                 .baseCurrencyId(baseCurrency)
@@ -77,7 +84,58 @@ public class ExchangeRateService {
                 .build();
     }
 
+    public Optional<CurrencyExchange> convertCurrency(
+            String baseCurrencyCode, String targetCurrencyCode, Double amount) throws SQLException {
 
-    public void convertCurrency(String baseCurrencyCode,String targetCurrencyCode,Double amount) {
+        Optional<ExchangeRate> currencyPair = exchangeRateDAO.findByCodes(baseCurrencyCode, targetCurrencyCode);
+        if (currencyPair.isPresent()) {
+             return directCurrencyExchange(currencyPair, amount);
+        }
+
+        Optional<ExchangeRate> ReverseCurrencyPair = exchangeRateDAO.findByCodes(
+                targetCurrencyCode, baseCurrencyCode);
+        if (ReverseCurrencyPair.isPresent()) {
+            return reverseCurrencyExchange(ReverseCurrencyPair, amount);
+        }
+
+        List<ExchangeRate> byCodesSeparately = exchangeRateDAO.findByCodesSeparately(
+                baseCurrencyCode, targetCurrencyCode);
+        if (!byCodesSeparately.isEmpty()) {
+            return crossCurrencyExchange(byCodesSeparately, amount);
+        }
+
+        return Optional.empty();
+    }
+
+    private Optional<CurrencyExchange> directCurrencyExchange(Optional<ExchangeRate> currencyPair, Double amount) {
+        ExchangeRate exchangeRate = currencyPair.get();
+        Double rate = exchangeRate.getRate();
+        return Optional.of(new CurrencyExchange(exchangeRate.getBaseCurrency(),
+                exchangeRate.getTargetCurrency(),
+                rate,
+                amount,
+                rate * amount));
+    }
+
+    private Optional<CurrencyExchange> reverseCurrencyExchange(Optional<ExchangeRate> currencyPair, Double amount) {
+        ExchangeRate exchangeRate = currencyPair.get();
+        Double rate = exchangeRate.getRate();
+        return Optional.of(new CurrencyExchange(exchangeRate.getTargetCurrency(),
+                exchangeRate.getBaseCurrency(),
+                1 / rate,
+                amount,
+                1 / rate * amount));
+    }
+
+    private Optional<CurrencyExchange> crossCurrencyExchange(List<ExchangeRate> currencyPairs, Double amount) {
+        ExchangeRate exchangeRate = currencyPairs.get(0);
+        Double firstRate = exchangeRate.getRate();
+        ExchangeRate secondExchangeRate = currencyPairs.get(1);
+        Double secondRate = secondExchangeRate.getRate();
+
+        return Optional.of(new CurrencyExchange(exchangeRate.getTargetCurrency(),
+                secondExchangeRate.getTargetCurrency(),
+                firstRate / secondRate, amount,
+                firstRate / secondRate * amount));
     }
 }
