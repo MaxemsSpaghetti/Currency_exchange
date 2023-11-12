a4df26f3ca02944cef206000ab0a44e3071566f3
MaxemsSpaghetti <maxbuten00@mail.ru>
2023-11-12 20:25:59 +0300

    feat: add findByCodeSeparately method

--- src/main/java/maxim/butenko/dao/ExchangeRateDAOImpl.java
+++ src/main/java/maxim/butenko/dao/ExchangeRateDAOImpl.java
@@ -4,9 +4,7 @@ import maxim.butenko.config.ConnectionManager;
 import maxim.butenko.model.Currency;
 import maxim.butenko.model.ExchangeRate;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -44,9 +42,9 @@ public class ExchangeRateDAOImpl implements ExchangeRateDAO {
 
     @Override
     public List<ExchangeRate> findAll() throws SQLException {
-        try (var connection = ConnectionManager.get()){
-            var prepareStatement = connection.prepareStatement(SQLQuery.FIND_ALL.QUERY);
-            var resultSet = prepareStatement.executeQuery();
+        try (Connection connection = ConnectionManager.get()){
+            PreparedStatement prepareStatement = connection.prepareStatement(SQLQuery.FIND_ALL.QUERY);
+            ResultSet resultSet = prepareStatement.executeQuery();
             List<ExchangeRate> exchangeRates = new ArrayList<>();
             while (resultSet.next()) {
                 exchangeRates.add(buildExchangeRate(resultSet));
@@ -58,11 +56,11 @@ public class ExchangeRateDAOImpl implements ExchangeRateDAO {
 
     @Override
     public Optional<ExchangeRate> findByCodes(String baseCurrencyCode, String targetCurrencyCode) throws SQLException {
-        try (var connection = ConnectionManager.get()){
-            var prepareStatement = connection.prepareStatement(SQLQuery.FIND_BY_CODE.QUERY);
+        try (Connection connection = ConnectionManager.get()){
+            PreparedStatement prepareStatement = connection.prepareStatement(SQLQuery.FIND_BY_CODES.QUERY);
             prepareStatement.setString(1, baseCurrencyCode);
             prepareStatement.setString(2, targetCurrencyCode);
-            var result = prepareStatement.executeQuery();
+            ResultSet result = prepareStatement.executeQuery();
 
             if (!result.next()) {
                 return Optional.empty();
@@ -71,10 +69,24 @@ public class ExchangeRateDAOImpl implements ExchangeRateDAO {
         }
     }
 
+    public List<ExchangeRate> findByCodesSeparately(String baseCurrencyCode, String targetCurrencyCode) throws SQLException {
+        try (Connection connection = ConnectionManager.get()) {
+            PreparedStatement prepareStatement = connection.prepareStatement(SQLQuery.FIND_BY_CODES_SEPARATELY.QUERY);
+            prepareStatement.setString(1, baseCurrencyCode);
+            prepareStatement.setString(2, targetCurrencyCode);
+            ResultSet result = prepareStatement.executeQuery();
+            List<ExchangeRate> exchangeRates = new ArrayList<>();
+            while (result.next()) {
+                exchangeRates.add(buildExchangeRate(result));
+            }
+            return exchangeRates;
+        }
+    }
+
     @Override
     public ExchangeRate update(ExchangeRate exchangeRate) throws SQLException {
-        try (var connection = ConnectionManager.get()){
-            var prepareStatement = connection.prepareStatement(SQLQuery.UPDATE.QUERY);
+        try (Connection connection = ConnectionManager.get()){
+            PreparedStatement prepareStatement = connection.prepareStatement(SQLQuery.UPDATE.QUERY);
             prepareStatement.setDouble(1, exchangeRate.getRate());
             prepareStatement.setObject(2, exchangeRate.getBaseCurrency().getId());
             prepareStatement.setObject(3, exchangeRate.getTargetCurrency().getId());
@@ -87,24 +99,25 @@ public class ExchangeRateDAOImpl implements ExchangeRateDAO {
     }
     @Override
     public ExchangeRate save(ExchangeRate exchangeRate) throws SQLException {
-
-        try (var connection = ConnectionManager.get()) {
+        try (Connection connection = ConnectionManager.get()) {
             connection.setAutoCommit(false);
-            var prepareStatement = connection.prepareStatement(SQLQuery.CREATE.QUERY, Statement.RETURN_GENERATED_KEYS);
+            PreparedStatement prepareStatement = connection.prepareStatement(SQLQuery.CREATE.QUERY, Statement.RETURN_GENERATED_KEYS);
             prepareStatement.setObject(1, exchangeRate.getBaseCurrency().getId());
             prepareStatement.setObject(2, exchangeRate.getTargetCurrency().getId());
             prepareStatement.setDouble(3, exchangeRate.getRate());
 
             prepareStatement.execute();
 
-            var newExchangeRate = prepareStatement.getGeneratedKeys();
+            ResultSet newExchangeRate = prepareStatement.getGeneratedKeys();
             newExchangeRate.next();
             exchangeRate.setId(newExchangeRate.getLong(1));
-            connection.commit(); // Завершить транзакцию
+            connection.commit();
             connection.setAutoCommit(true);
         }
         return exchangeRate;
     }
+
+
 
     enum SQLQuery {
         FIND_ALL("""
@@ -120,12 +133,12 @@ public class ExchangeRateDAOImpl implements ExchangeRateDAO {
                     targetCurrency.sign AS target_sign,
                     exchangeRate.rate AS rate
                 FROM exchangeRates exchangeRate
-                JOIN currencies baseCurrency ON exchangeRate.baseCurrencyId = baseCurrency.id
-                JOIN currencies targetCurrency ON exchangeRate.targetCurrencyId = targetCurrency.id
+                    JOIN currencies baseCurrency ON exchangeRate.baseCurrencyId = baseCurrency.id
+                    JOIN currencies targetCurrency ON exchangeRate.targetCurrencyId = targetCurrency.id
                 ORDER BY exchangeRate.id
             """),
 
-        FIND_BY_CODE("""
+        FIND_BY_CODES("""
                 SELECT
                 exchangeRate.id AS id,
                 baseCurrency.id AS base_id,
@@ -138,13 +151,35 @@ public class ExchangeRateDAOImpl implements ExchangeRateDAO {
                 targetCurrency.sign AS target_sign,
                 exchangeRate.rate AS rate
                 FROM exchangeRates exchangeRate
-                JOIN currencies baseCurrency ON exchangeRate.baseCurrencyId = baseCurrency.id
-                JOIN currencies targetCurrency ON exchangeRate.targetCurrencyId = targetCurrency.id
+                    JOIN currencies baseCurrency ON exchangeRate.baseCurrencyId = baseCurrency.id
+                    JOIN currencies targetCurrency ON exchangeRate.targetCurrencyId = targetCurrency.id
                 WHERE (
                     baseCurrencyId = (SELECT currency.id FROM currencies currency WHERE currency.code = ?) AND
-                    targetCurrencyId = (SELECT currency.id FROM currencies currency WHERE currency.code = ?)   
+                    targetCurrencyId = (SELECT currency.id FROM currencies currency WHERE currency.code = ?)  
                 )    
             """),
+
+        FIND_BY_CODES_SEPARATELY("""
+                SELECT
+                    exchangeRate.id AS id,
+                    baseCurrency.id AS base_id,
+                    baseCurrency.fullName AS base_fullName,
+                    baseCurrency.code AS base_code,
+                    baseCurrency.sign AS base_sign,
+                    targetCurrency.id AS target_id,
+                    targetCurrency.fullName AS target_fullName,
+                    targetCurrency.code AS target_code,
+                    targetCurrency.sign AS target_sign,
+                    exchangeRate.rate AS rate
+                FROM exchangeRates exchangeRate
+                    JOIN currencies baseCurrency ON exchangeRate.baseCurrencyId = baseCurrency.id
+                    JOIN currencies targetCurrency ON exchangeRate.targetCurrencyId = targetCurrency.id
+                WHERE (
+                    baseCurrencyId = (SELECT currency.id FROM currencies currency WHERE currency.code = 'USD') AND
+                    (targetCurrencyId = (SELECT currency1.id FROM currencies currency1 WHERE currency1.code = ?) OR
+                    targetCurrencyId = (SELECT currency2.id FROM currencies currency2 WHERE currency2.code = ?))
+                )
+                """),
 
         CREATE("INSERT INTO exchangeRates (baseCurrencyId, targetCurrencyId, rate) VALUES (?, ?, ?)"),
 
