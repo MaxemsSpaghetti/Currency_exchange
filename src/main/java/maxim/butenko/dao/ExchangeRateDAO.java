24f153d565a436de693ed67adf6d763e7cc4a89a
MaxemsSpaghetti <maxbuten00@mail.ru>
2023-11-13 21:27:13 +0300

    feat: add findByCodesSeparately method

--- src/main/java/maxim/butenko/dao/ExchangeRateDAO.java
+++ src/main/java/maxim/butenko/dao/ExchangeRateDAO.java
@@ -3,8 +3,11 @@ package maxim.butenko.dao;
 import maxim.butenko.model.ExchangeRate;
 
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Optional;
 
 public interface ExchangeRateDAO extends DAO<ExchangeRate> {
     Optional<ExchangeRate> findByCodes(String baseCurrencyCode, String targetCurrencyCode) throws SQLException;
+
+    List<ExchangeRate> findByCodesSeparately(String baseCurrencyCode, String targetCurrencyCode) throws SQLException;
 }
