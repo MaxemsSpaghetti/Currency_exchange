1576147a9f2fa98db87251391406aa2d03d7c4fb
MaxemsSpaghetti <maxbuten00@mail.ru>
2023-11-13 21:25:25 +0300

    fix: redid the error display to json

--- src/main/java/maxim/butenko/servlet/CurrencyExchangeServlet.java
+++ src/main/java/maxim/butenko/servlet/CurrencyExchangeServlet.java
@@ -1,10 +1,10 @@
 package maxim.butenko.servlet;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import maxim.butenko.ErrorResponse;
 import maxim.butenko.model.CurrencyExchange;
-import maxim.butenko.service.ExchangeRateService;
+import maxim.butenko.service.CurrencyExchangeService;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,12 +16,12 @@ import java.util.Optional;
 @WebServlet("/exchange")
 public class CurrencyExchangeServlet extends HttpServlet {
 
-    private final ExchangeRateService exchangeRateService = ExchangeRateService.getInstance();
+    private final CurrencyExchangeService currencyExchangeService = CurrencyExchangeService.getInstance();
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
 
@@ -33,35 +33,53 @@ public class CurrencyExchangeServlet extends HttpServlet {
             amount = Double.valueOf(req.getParameter("amount"));
         } catch (NumberFormatException e) {
             resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            resp.sendError(HttpServletResponse.SC_BAD_REQUEST,"amount is required");
+            objectMapper.writeValue(resp.getWriter(), new ErrorResponse(
+                    HttpServletResponse.SC_BAD_REQUEST,
+                    "amount is required"));
             return;
+
         }
         if (baseCurrencyCode.isBlank()) {
             resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            resp.sendError(HttpServletResponse.SC_BAD_REQUEST,
-                    "baseCurrencyCode is required");
+            objectMapper.writeValue(resp.getWriter(), new ErrorResponse(
+                    HttpServletResponse.SC_BAD_REQUEST,
+                    "baseCurrencyCode is required"));
             return;
+
         } else if (targetCurrencyCode.isBlank()) {
             resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            resp.sendError(HttpServletResponse.SC_BAD_REQUEST,
-                    "targetCurrencyCode is required");
+            objectMapper.writeValue(resp.getWriter(), new ErrorResponse(
+                    HttpServletResponse.SC_BAD_REQUEST,
+                    "targetCurrencyCode is required"));
+            return;
+
+        } else if (baseCurrencyCode.equals(targetCurrencyCode)) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            objectMapper.writeValue(resp.getWriter(), new ErrorResponse(
+                    HttpServletResponse.SC_BAD_REQUEST,
+                    "baseCurrencyCode and targetCurrencyCode cannot be the same"));
             return;
         }
+
         try {
-            Optional<CurrencyExchange> convertedCurrency = exchangeRateService.convertCurrency(baseCurrencyCode, targetCurrencyCode, amount);
+            Optional<CurrencyExchange> convertedCurrency = currencyExchangeService
+                    .convertCurrency(baseCurrencyCode, targetCurrencyCode, amount);
             if (convertedCurrency.isPresent()) {
                 CurrencyExchange currencyExchange = convertedCurrency.get();
                 String json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(currencyExchange);
                 resp.getWriter().write(json);
             } else {
                 resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-                resp.sendError(HttpServletResponse.SC_NOT_FOUND,
-                        "There are no exchange rates for the specified currencies in the list of exchange rates");
+                objectMapper.writeValue(resp.getWriter(), new ErrorResponse(
+                        HttpServletResponse.SC_NOT_FOUND,
+                        "There are no exchange rates " +
+                                "for the specified currencies in the list of exchange rates"));
             }
         } catch (SQLException e) {
             resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
-                    "the database is unavailable now, sorry(");
+            objectMapper.writeValue(resp.getWriter(), new ErrorResponse(
+                    HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+                    "the database is unavailable now, sorry("));
         }
     }
 }
