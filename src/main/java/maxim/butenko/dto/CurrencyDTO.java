254008d4ea6b4a6ca23201b2462fd656ec9a546c
MaxemsSpaghetti <maxbuten00@mail.ru>
2023-11-13 21:26:45 +0300

    fix: delete @JsonRawValue annotation and add @JsonProperty annotation

--- src/main/java/maxim/butenko/dto/CurrencyDTO.java
+++ src/main/java/maxim/butenko/dto/CurrencyDTO.java
@@ -1,15 +1,18 @@
 package maxim.butenko.dto;
 
-import com.fasterxml.jackson.annotation.JsonRawValue;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Builder;
 import lombok.Value;
 
 @Value
 @Builder
+@JsonPropertyOrder({"id", "name", "code", "sign"})
 public class CurrencyDTO {
     Long id;
+    @JsonProperty("name")
     String fullName;
     String code;
-    @JsonRawValue
     String sign;
+
 }
