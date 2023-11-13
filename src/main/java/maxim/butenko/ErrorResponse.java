72f4043b96bbf71272d7790568435a6410cefecd
MaxemsSpaghetti <maxbuten00@mail.ru>
2023-11-13 21:29:00 +0300

    fix: add ErrorResponse

---
+++ src/main/java/maxim/butenko/ErrorResponse.java
@@ -0,0 +1,11 @@
+package maxim.butenko;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorResponse {
+    private int error;
+    private String message;
+}
