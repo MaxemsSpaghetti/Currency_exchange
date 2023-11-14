e37bb2ea14b4b9d9ec44f367dcd728dd0ec54e67
MaxemsSpaghetti <maxbuten00@mail.ru>
2023-11-14 21:38:59 +0300

    fix(after selfReview): delete builder annotation because of no usage

--- src/main/java/maxim/butenko/model/Currency.java
+++ src/main/java/maxim/butenko/model/Currency.java
@@ -1,17 +1,21 @@
 package maxim.butenko.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 public class Currency {
     private Long id;
     private String fullName;
     private String code;
     private String sign;
+
+    public Currency(String fullName, String code, String sign) {
+        this.fullName = fullName;
+        this.code = code;
+        this.sign = sign;
+    }
 }
