728a5800f689acec830a9545beaffd4e616ff3e7
MaxemsSpaghetti <maxbuten00@mail.ru>
2023-11-20 21:00:52 +0300

    fix: update ConnectionManager

--- src/main/java/maxim/butenko/config/ConnectionManager.java
+++ src/main/java/maxim/butenko/config/ConnectionManager.java
@@ -2,10 +2,18 @@ package maxim.butenko.config;
 
 import lombok.SneakyThrows;
 
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Objects;
 
-public final class ConnectionManager {
+public class ConnectionManager {
+
+    private static final String DATABASE_NAME = "currency_exchange.db";
+    private static String path;
+
 
     static {
         loadDriver();
@@ -13,17 +21,23 @@ public final class ConnectionManager {
 
     private static void loadDriver() {
         try {
+            URL dbUrl = ConnectionManager.class.getClassLoader().getResource(DATABASE_NAME);
+
+            try {
+                path = new File(Objects.requireNonNull(dbUrl).toURI()).getAbsolutePath();
+            } catch (URISyntaxException e) {
+                throw new RuntimeException(e);
+            }
+
             Class.forName("org.sqlite.JDBC");
-        } catch (ClassNotFoundException e) {
+
+        } catch (RuntimeException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private ConnectionManager() {
-
-    }
     @SneakyThrows
     public static Connection get() {
-        return DriverManager.getConnection("jdbc:sqlite:C:/Users/Jew/SQLite/currency_exchange.db");
+        return DriverManager.getConnection(String.format("jdbc:sqlite:%s", path));
     }
 }
